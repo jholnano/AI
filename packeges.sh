1268df62e4db520f214cc62b1cadf06d2d2c8b11
Jholnano <50936327+nanojhol@users.noreply.github.com>
2023-02-20 14:18:06 -0300

    Bibliotecas necessárias

---
+++ packeges.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Frameworks
+pip install pysound
+pip install customtkinter
+pip install openai
+pip install --upgrade openai
+pip install openai[embeddings]
+pip install openai[wandb]
+pip install openai[datalib]
+pip install pillow
+pip install gTTS
+pip install SpeechRecognition
+pip install playsound
+pip install --upgrade pip
